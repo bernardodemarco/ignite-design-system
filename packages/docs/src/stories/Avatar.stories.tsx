1ee9a0208917fc5317f5545b8a7cbe93a500f3ba
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-07-05 16:29:25 -0300

    avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://www.github.com/bernardodemarco.png',
+    alt: 'Bernardo De Marco Gonçalves',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
